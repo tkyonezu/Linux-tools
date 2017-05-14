8d5ca07011ebd7d988d4d249f4346debc4031e46
Sleepy Senzu <tkyonezu@gmail.com>
2017-05-14 05:16:24 +0000

    Support both dockerproject.org and doker.com version

--- ins-docker.sh
+++ ins-docker.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
+# Install Official Docker version
+#
+# Get Docker for Ubuntu
+# https://docs.docker.com/engine/installation/linux/ubuntu/
+#
+# Get Docker for Debian
+# Jessie 8.0 (LTD)/Raspbian jessie
+# https://docs.docker.com/engine/installation/linux/debian/
+#
+# To install fixed version for Production
+# https://docs.docker.com/engine/installation/linux/ubuntu/
+#
+#   apt-cache madison docker-ce
+#   apt install -y docker-ce=<VERSION>
+
+DOCKER_REPO="dockerproject.org"		# dockerproject.org or docker.com
+DOCKER_PKG="docker-engine"		# docker-engine or docker-ce
+
 if [[ "$(uname -s)" = "Linux" && "$(uname -m)" = "x86_64" ]]; then
   echo "=== Linux ==="
-  PLATFORM="amd64"
+  ARCH="amd64"
 elif [[ "$(uname -s)" = "Darwin" ]]; then
   echo "=== MacOS ==="
-  PLATFORM="Mac"
+  ARCH="amd64"
   echo "Get Docker for Mac(stable) from https://download.docker.com/mac/stable/Docker.dmg"
   echo "and Install it."
   exit 0
 elif [[ "$(uname -s)" = "Linux" && "$(uname -m)" = "armv7l" ]]; then
   echo "=== Raspberry Pi ==="
-  PLATFORM="armhf"
+  ARCH="armhf"
 else
   echo "This platform does'nt support yet."
   exit 1
@@ -21,67 +39,37 @@ apt remove -y docker docker-engine docker-ce
 
 apt update
 
-if [[ "$PLATFORM" = "armhf" ]]; then
-  apt install -y apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common
+apt install -y apt-transport-https \
+  ca-certificates \
+  curl \
+  software-properties-common
 
-  curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+if [[ ${DOCKER_REPO} = "dockerproject.org" ]]; then
+  curl -fsSL https://apt.dockerproject.org/gpg | apt-key add -
 
-  apt-key fingerprint 0EBFCD88
-
-  echo "deb [arch=armhf] https://apt.dockerproject.org/repo \
-    raspbian-$(lsb_release -cs) main" | \
-    tee /etc/apt/sources.list.d/docker.list
-
-    apt update
-
-    apt install -y docker-engine
-
-## # Official docker version "Get Docker for Ubuntu"
-## # https://docs.docker.com/engine/installation/linux/ubuntu/
-##
-## echo "deb [arch=armhf] https://download.docker.com/linux/debian \
-##   $(lsb_release -cs) stable" | \
-##   tee /etc/apt/sources.list.d/docker.list
-## 
-## apt update
-## 
-## apt install -y docker-ce
+  if [[ "${ARCH}" = "amd64" ]]; then
+    echo "deb [arch=${ARCH}] https://apt.dockerproject.org/repo \
+      ubuntu-$(lsb_release -cs) main" | \
+      tee /etc/apt/sources.list.d/docker.list
+  else
+    echo "deb [arch=${ARCH}] https://apt.dockerproject.org/repo \
+      raspbian-$(lsb_release -cs) main" | \
+      tee /etc/apt/sources.list.d/docker.list
+  fi
 else
-  apt install -y apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+  if [[ "${ARCH}" = "amd64" ]]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+  else
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+  fi
 
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-  apt-key fingerprint 0EBFCD88
-
-  add-apt-repository \
-    "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
-
-  apt update
-
-  apt install -y docker-engine
-
-## # Official docker version "Get Docker for Ubuntu"
-## # https://docs.docker.com/engine/installation/linux/ubuntu/
-## 
-## add-apt-repository \
-##   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-##   $(lsb_release -cs) stable"
-## 
-## apt update
-## 
-## apt install -y docker-ce
+  echo "deb [arch=${ARCH}] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | \
+    tee /etc/apt/sources.list.d/docker.list
 fi
 
-## # To install fixed version for Production
-## # https://docs.docker.com/engine/installation/linux/ubuntu/
-## apt-cache madison docker-ce
-## apt install -y docker-ce=<VERSION>
+apt update
+
+apt install -y ${DOCKER_PKG}
 
 exit 0
