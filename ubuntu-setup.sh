b7c1a463233bf91b06b194cf6625c509b437b761
Sleepy Senzu <tkyonezu@gmail.com>
2018-02-27 10:24:39 +0900

    Correct typo and delete to add swapfile

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -36,7 +36,7 @@ apt install -y curl git make htop
 
 # Install docker-ce
 logmsg "2. Install docker-ce"
-apt instal -y apt-transport-https ca-certificates curl \
+apt install -y apt-transport-https ca-certificates curl \
   software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -53,12 +53,12 @@ logmsg "3. Install docker-compose"
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Add swapfile
-logmsg "4. Add swapfile"
-dd if=/dev/zero of=/swapfile bs=1M count=4096
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
+## # Add swapfile
+## logmsg "4. Add swapfile"
+## dd if=/dev/zero of=/swapfile bs=1M count=4096
+## chmod 600 /swapfile
+## mkswap /swapfile
+## swapon /swapfile
 
 # Set hostname
 logmsg "5. Set hostname (${NEW_HOST})"
