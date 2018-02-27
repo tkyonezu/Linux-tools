be9721b3c32497fccbe557a44438e2a6185be801
Sleepy Senzu <tkyonezu@gmail.com>
2018-02-27 17:52:54 +0900

    Enable to add swapfile.

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -58,52 +58,57 @@ logmsg "3. Install docker-compose"
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-## # Add swapfile
-## logmsg "4. Add swapfile"
-## dd if=/dev/zero of=/swapfile bs=1M count=4096
-## chmod 600 /swapfile
-## mkswap /swapfile
-## swapon /swapfile
-
 # Set hostname
-logmsg "5. Set hostname (${NEW_HOST})"
+logmsg "4. Set hostname (${NEW_HOST})"
 echo ${NEW_HOST} >/etc/hostname
 
 # Change timezone
-logmsg "6. Change timezone (Asia/Tokyo)"
+logmsg "5. Change timezone (Asia/Tokyo)"
 timedatectl set-timezone Asia/Tokyo
 
 # Setup user
-logmsg "7. Setup user (${NEW_USER})"
-if [ "${NEW_USER}" != "ubuntu" ]; then
+logmsg "6. Setup user (${NEW_USER})"
+if [ "${NEW_USER}" = "ubuntu" ]; then
+  logmsg "ubuntu user don't do anything."
+else
   if ! grep -q "^${NEW_USER}" /etc/passwd; then
     useradd -ms /bin/bash ${NEW_USER}
   fi
+
+  NEW_UID=$(id -u ${NEW_USER})
+  NEW_GID=$(id -g ${NEW_USER})
 
   if [ ! -f /etc/sudoers.d/010_${NEW_USER}-nopasswd ]; then
     echo "${NEW_USER} ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/010_${NEW_USER}-nopasswd
     chmod 440 /etc/sudoers.d/010_${NEW_USER}-nopasswd
   fi
+
+  cd $(grep ^${NEW_USER} /etc/passwd | cut -d':' -f6)
+  if [ ! -d .ssh ]; then
+    mkdir -p .ssh
+    chown ${NEW_UID}:${NEW_GID} .ssh
+    chmod 700 .ssh
+  fi
+  if [ ! -f .ssh/authorized_keys ]; then
+    > .ssh/authorized_keys
+    chown ${NEW_UID}:${NEW_GID} .ssh/authorized_keys
+    chmod 600 .ssh/authorized_keys
+  fi
 fi
 
-NEW_UID=$(id -u ${NEW_USER})
-NEW_GID=$(id -g ${NEW_USER})
+# Add swapfile
+if [ $(swapon -s | wc -l) -eq 0 ]; then
+  logmsg "7. Add swapfile"
+  logmsg "Create 4GB of swapfile and add it to /etc/fstab"
+  dd if=/dev/zero of=/swapfile bs=1M count=4096
+  chmod 600 /swapfile
 
-cd $(grep ^${NEW_USER} /etc/passwd | cut -d':' -f6)
-if [ ! -d .ssh ]; then
-  mkdir -p .ssh
-  chown ${NEW_UID}:${NEW_GID} .ssh
-  chmod 700 .ssh
-fi
+  mkswap /swapfile
+  swapon /swapfile
 
-if [ ! -f .ssh/authorized_keys ]; then
-  > .ssh/authorized_keys
-  chown ${NEW_UID}:${NEW_GID} .ssh/authorized_keys
-  chmod 600 .ssh/authorized_keys
-fi
-
-if ! id -Gn ${NEW_USER} | grep -q docker; then
-  usermod -aG docker ${NEW_USER}
+  logmsg "Add the following entry to /etc/fstab"
+  UUID=$(blkid /swapfile | sed -e 's/^.*UUID="//' -e 's/" TYPE=.*//')
+  echo "UUID=${UUID} none            swap    sw              0       0" | tee -a /etc/fstab
 fi
 
 # Update ubuntu
