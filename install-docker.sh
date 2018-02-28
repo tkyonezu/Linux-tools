0d8eeb8c0ae57b81eb5df664126abcec106ba635
Sleepy Senzu <tkyonezu@gmail.com>
2018-02-28 23:08:07 +0900

    ins-docker.sh rename to install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Install Official Docker version
+#
+# Get Docker for Ubuntu
+# https://docs.docker.com/engine/installation/linux/ubuntu/
+#
+# Get Docker for Debian
+# Jessie 8.0 (LTD)/Raspbian jessie
+# https://docs.docker.com/engine/installation/linux/debian/
+#
+# To install fixed version for Production
+# https://docs.docker.com/engine/installation/linux/ubuntu/
+#
+#   apt-cache madison docker-ce
+#   apt install -y docker-ce=<VERSION>
+
+DOCKER_REPO="dockerproject.org"		# dockerproject.org or docker.com
+DOCKER_PKG="docker-engine"		# docker-engine or docker-ce
+
+if [[ "$(uname -s)" = "Linux" && "$(uname -m)" = "x86_64" ]]; then
+  echo "=== Linux ==="
+  ARCH="amd64"
+elif [[ "$(uname -s)" = "Darwin" ]]; then
+  echo "=== MacOS ==="
+  ARCH="amd64"
+  echo "Get Docker for Mac(stable) from https://download.docker.com/mac/stable/Docker.dmg"
+  echo "and Install it."
+  exit 0
+elif [[ "$(uname -s)" = "Linux" && "$(uname -m)" = "armv7l" ]]; then
+  echo "=== Raspberry Pi ==="
+  ARCH="armhf"
+else
+  echo "This platform does'nt support yet."
+  exit 1
+fi
+
+apt remove -y docker docker-engine docker-ce
+
+apt update
+
+apt install -y apt-transport-https \
+  ca-certificates \
+  curl \
+  software-properties-common
+
+if [[ ${DOCKER_REPO} = "dockerproject.org" ]]; then
+  curl -fsSL https://apt.dockerproject.org/gpg | apt-key add -
+
+  if [[ "${ARCH}" = "amd64" ]]; then
+    echo "deb [arch=${ARCH}] https://apt.dockerproject.org/repo \
+      ubuntu-$(lsb_release -cs) main" | \
+      tee /etc/apt/sources.list.d/docker.list
+  else
+    echo "deb [arch=${ARCH}] https://apt.dockerproject.org/repo \
+      raspbian-$(lsb_release -cs) main" | \
+      tee /etc/apt/sources.list.d/docker.list
+  fi
+else
+  if [[ "${ARCH}" = "amd64" ]]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+  else
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+  fi
+
+  echo "deb [arch=${ARCH}] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | \
+    tee /etc/apt/sources.list.d/docker.list
+fi
+
+apt update
+
+apt install -y ${DOCKER_PKG}
+
+exit 0
