0460ff1b5021ca652ae2e29e69066773bd66d290
Sleepy Senzu <tkyonezu@gmail.com>
2018-12-01 00:46:05 +0900

    docker-compose version upgrade to 1.23.2

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,7 +3,7 @@
 # Install Docker Compose
 # https://docs.docker.com/compose/install/#install-compose
 
-COMPOSE_VERSION=1.23.1
+COMPOSE_VERSION=1.23.2
 
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
