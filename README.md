5c7b1e29d5d81e903387c245adc70ca7f758aa35
Sleepy Senzu <tkyonezu@gmail.com>
2018-12-21 17:11:56 +0900

    Add to install ctop.

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 | apt-update.sh | apt update remote hosts using by ssh |
 | centos-setup.sh | CentOS 7 first setup script |
 | docker-compose.sh | Install docker-compose (amd64, Darwin) |
+| install-ctop.sh | Install ctop (Linux/x86_64, Darwin/x86_64, Linux/armv7l) |
 | install-docker.sh | Install Docker (amd64, Darwin, armhf) |
 | install-go.sh | Install Go (Linux/x86_64, Darwin/x86_64, Linux/armv7l) |
 | install-node.sh | Install Node.js 8.x |
---
+++ install-ctop.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Install ctop
+# https://github.com/bcicen/ctop/releases
+
+CTOP_VERSION=0.7.1
+
+HARDWARE=$(uname -m)
+SYSTEM=$(uname -s)
+
+if [ "${SYSTEM}" = "Linux" ]; then
+  if [ "${HARDWARE}" = "armv7l" ]; then
+    SYSTEM="linux"
+    HARDWARE="arm64"
+  elif [ "${HARDWARE}" = "x86_64" ]; then
+    SYSTEM="linux"
+    HARDWARE="amd64"
+  else
+    echo "This platform does'nt suppot yet!!"
+    exit 1
+  fi
+elif [ "${SYSTEM}" = "Darwin" ]; then
+  if [ "${HARDWARE}" = "x86_64" ]; then
+    SYSTEM="darwin"
+    HARDWARE="amd64"
+  else
+    echo "This platform does'nt suppot yet!!"
+    exit 1
+  fi
+else
+  echo "This platform does'nt suppot yet!!"
+  exit 1
+fi
+
+curl -L https://github.com/bcicen/ctop/releases/download/v${CTOP_VERSION}/ctop-${CTOP_VERSION}-${SYSTEM}-${HARDWARE} -o /usr/local/bin/ctop
+
+chmod +x /usr/local/bin/ctop
+
+exit 0
