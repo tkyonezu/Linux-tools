db563fbb895d0da1a18c277d86cedb39117e838b
Sleepy Senzu <tkyonezu@gmail.com>
2019-03-11 01:43:16 +0900

    Copy from raspberry/rpi-setup/install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,94 +1,54 @@
 #!/bin/bash
 
-# Install Official Docker version
+#-----------------------------------------------------------------------
+# Raspberry Pi 3
 #
-# Get Docker for Ubuntu
-# https://docs.docker.com/engine/installation/linux/ubuntu/
+# install-docker.sh - Install Docker-Engine
 #
-# Get Docker for Debian
-# Jessie 8.0 (LTD)/Raspbian jessie
-# https://docs.docker.com/engine/installation/linux/debian/
+# usage: install-docker.sh
 #
-# To install fixed version for Production
-# https://docs.docker.com/engine/installation/linux/ubuntu/
+# Copyright (c) 2017 Takeshi Yonezu
+# All Rights Reserved.
+#-----------------------------------------------------------------------
+
+logmsg() {
+  echo ">>> $1"
+}
+
 #
-#   apt-cache madison docker-ce
-#   apt install -y docker-ce=<VERSION>
+# Install Docker
+#
+logmsg "Install Docker"
 
-## DOCKER_REPO="dockerproject.org"	# dockerproject.org or docker.com
-## DOCKER_PKG="docker-engine"		# docker-engine or docker-ce
-DOCKER_REPO="docker.com"		# dockerproject.org or docker.com
-DOCKER_PKG="docker-ce"			# docker-engine or docker-ce
+OS=$(uname -s)
+ARCH=$(uname -m)
 
-if [[ "$(uname -s)" = "Linux" && "$(uname -m)" = "x86_64" ]]; then
-  echo "=== Linux ==="
-  ARCH="amd64"
-elif [[ "$(uname -s)" = "Darwin" ]]; then
-  echo "=== MacOS ==="
-  ARCH="amd64"
-  echo "Get Docker for Mac(stable) from https://download.docker.com/mac/stable/Docker.dmg"
-  echo "and Install it."
-  exit 0
-elif [[ "$(uname -s)" = "Linux" && "$(uname -m)" = "armv7l" ]]; then
-  echo "=== Raspberry Pi ==="
-  ARCH="armhf"
-else
-  echo "This platform does'nt support yet."
-  exit 1
-fi
+case ${OS} in
+  Linux)  OS=linux;;
+  Darwin) echo "${OS}/${ARCH} can use Docker for Mac."; exit 1;;
+  *) echo "${OS}-${ARCH} does'nt supported yet."; exit 1;;
+esac
 
-OS_NAME=$(cat /etc/os-release | grep ^NAME | cut -d'"' -f2 | sed 's/ .*//')
+case ${ARCH} in
+  x86_64) ARCH=amd64; DIST=ubuntu;;
+# armv7l) ARCH=armhf; DIST=raspbian;;
+  armv7l) ARCH=armhf; DIST=debian;;
+  *) echo "${OS}-${ARCH} does'nt supported yet."; exit 1;;
+esac
 
-if [ "${OS_NAME}" = "CentOS" ]; then
-  yum install -y yum-utils device-mapper-persistent-data lvm2
+apt install -y apt-transport-https ca-certificates curl \
+  software-properties-common
 
-  yum-config-manager --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+curl -fsSL https://download.docker.com/linux/${DIST}/gpg | sudo apt-key add -
 
-  yum install -y docker-ce
+apt-key fingerprint 0EBFCD88
 
-  systemctl start docker
-  systemctl enable docker
-else
-  apt remove -y docker docker-engine docker-ce
-  
-  apt update
-  
-  apt install -y apt-transport-https ca-certificates \
-    curl gnupg2 software-properties-common
-  
-  if [[ ${DOCKER_REPO} = "dockerproject.org" ]]; then
-    curl -fsSL https://apt.dockerproject.org/gpg | apt-key add -
-  
-    if [[ "${ARCH}" = "amd64" ]]; then
-      echo "deb [arch=${ARCH}] https://apt.dockerproject.org/repo \
-        ubuntu-$(lsb_release -cs) main" | \
-        tee /etc/apt/sources.list.d/docker.list
-    else
-      echo "deb [arch=${ARCH}] https://apt.dockerproject.org/repo \
-        raspbian-$(lsb_release -cs) main" | \
-        tee /etc/apt/sources.list.d/docker.list
-    fi
-  else
-    if [[ "${ARCH}" = "amd64" ]]; then
-      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/docker.list
+deb [arch=${ARCH}] https://download.docker.com/linux/${DIST} $(lsb_release -cs) stable
+EOF
 
-      echo "deb [arch=${ARCH}] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | \
-        tee /etc/apt/sources.list.d/docker.list
-    else
-      curl -fsSL https://download.docker.com/linux/raspbian/gpg | apt-key add -
+apt update
 
-      echo "deb [arch=${ARCH}] https://download.docker.com/linux/raspbian \
-        $(lsb_release -cs) stable" | \
-        tee /etc/apt/sources.list.d/docker.list
-    fi
-  
-  fi
-  
-  apt update
-  
-  apt install -y ${DOCKER_PKG}
-fi
+apt install -y docker-ce
 
 exit 0
