cab6763fdc26388160d93efb4a3b5ce62569abde
Sleepy Senzu <tkyonezu@gmail.com>
2020-02-04 01:06:00 +0900

    docker-compose upgrade to 1.25.4

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,7 +3,7 @@
 # Install Docker Compose
 # https://docs.docker.com/compose/install/#install-compose
 
-COMPOSE_VERSION=1.25.1
+COMPOSE_VERSION=1.25.4
 
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
