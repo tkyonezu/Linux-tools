4e9fd23fdfeb62b9dcb386485e572e9e00a34a30
Sleepy Senzu <tkyonezu@gmail.com>
2020-09-18 17:11:48 +0900

    docker-compose Upgrade to 1.27.3

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,7 +3,7 @@
 # Install Docker Compose
 # https://docs.docker.com/compose/install/#install-compose
 
-COMPOSE_VERSION=1.27.2
+COMPOSE_VERSION=1.27.3
 
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
