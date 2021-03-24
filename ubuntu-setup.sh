006efc138971bdbe3dfb16a7538005143dc32151
Takeshi Yonezu <tkyonezu@gmail.com>
2021-03-25 00:06:30 +0900

    docker-compose Upgrade to 1.28.6

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -19,7 +19,7 @@ fi
 SWAP_SIZE_G=4				# 4GB
 SWAP_SIZE_M=$((${SWAP_SIZE_G} * 1024))
 
-COMPOSE_VERSION=1.28.5
+COMPOSE_VERSION=1.28.6
 
 function install-docker-compose {
   curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
