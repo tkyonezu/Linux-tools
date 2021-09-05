a758e5246bf2b4515de54495a3b60e68b92d4b14
Takeshi Yonezu <tkyonezu@gmail.com>
2021-09-05 23:06:26 +0900

    Support 1.29.2 and v2.0.0-rc.3

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,10 +3,51 @@
 # Install Docker Compose
 # https://docs.docker.com/compose/install/#install-compose
 
-COMPOSE_VERSION=1.29.2
+RELEASE_URL=https://github.com/docker/compose/releases/download
+DEST_DIR=/usr/local/bin
 
-curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+## VERSION=v2.0.0-rc.3
+VERSION=1.29.2
 
-chmod +x /usr/local/bin/docker-compose
+if [ "$(echo ${VERSION} | cut -c1,2)" = "v2" ]; then
+  case $(uname -s) in
+    Darwin)  SYS=darwin;; # Mac OS Intel and Apple M1
+    Linux)   SYS=linux;;  # Linux 64bit include Raspberry Pi OS
+    *) echo "$(uname -s)/$(uname -m) does'nt supported."; exit 1;;
+  esac
 
-/usr/local/bin/docker-compose --version
+  case $(uname -m) in
+    x86_64)  ARC=amd64;;  # Mac OS Intel and Linux 64bit
+    arm64)   ARC=arm64;;  # Mac OS Apple M1
+    aarch64) ARC=arm64;;  # Raspberry Pi OS 64bit and Ubuntu 64bit
+    armv7l)  ARC=armv7;;  # Raspberry Pi OS 32bit 
+    *) echo "$(uname -s)/$(uname -m) does'nt supported."; exit 1;;
+  esac
+
+  sudo curl -L ${RELEASE_URL}/${VERSION}/docker-compose-${SYS}-${ARC} \
+    -o ${DEST_DIR}/docker-compose-v2
+
+  sudo chmod +x ${DEST_DIR}/docker-compose-v2
+
+  ${DEST_DIR}/docker-compose-v2 compose version
+else
+  case $(uname -s) in
+    Darwin)  SYS=Darwin;;
+    Linux)   SYS=linux;;
+    *) echo "$(uname -s)/$(uname -m) does'nt supported."; exit 1;;
+  esac
+
+  case $(uname -m) in
+    x86_64)  ARC=x86_64;;
+    *) echo "$(uname -s)/$(uname -m) does'nt supported."; exit 1;;
+  esac
+
+  sudo curl -L ${RELEASE_URL}/${VERSION}/docker-compose-${SYS}-${ARC} \
+    -o ${DEST_DIR}/docker-compose
+
+  sudo chmod +x ${DEST_DIR}/docker-compose
+
+  ${DEST_DIR}/docker-compose version
+fi
+
+exit 0
