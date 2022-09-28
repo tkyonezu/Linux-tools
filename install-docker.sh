4e52590277b5425fba81658e54cf173ed8d4f7ca
Takeshi Yonezu <tkyonezu@gmail.com>
2022-09-28 09:16:51 +0900

    Fixed support for Linux distributions other than Debian

--- install-docker.sh
+++ install-docker.sh
@@ -43,14 +43,14 @@ sudo apt install -y ca-certificates curl gnupg lsb-release
 ## curl -fsSL https://download.docker.com/linux/${DIST}/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/${DIST}/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 ## cat <<EOF | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 ## deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DIST} $(lsb_release -cs) stable
 ## EOF
 
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/${DIST} \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
