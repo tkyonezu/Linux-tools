8a96c8712ca1a6ccc19bde58decc98b43d9258e8
Takeshi Yonezu <tkyonezu@gmail.com>
2022-10-02 18:31:32 +0900

    Add arch=arm64 to triivy.list as it will warn you that it doesn't support arhmf.

--- install-trivy.sh
+++ install-trivy.sh
@@ -4,7 +4,7 @@ sudo apt install -y wget apt-transport-https gnupg lsb-release
 
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
 
-echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+echo "deb [arch=arm64 signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
 
 sudo apt update
 sudo apt install -y trivy
